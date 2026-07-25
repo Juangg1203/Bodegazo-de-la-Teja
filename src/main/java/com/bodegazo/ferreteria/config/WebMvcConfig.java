806db0a9292559527b4a89d7ca36445f2346d22e
Juangg1203 <jcgg1204@gmail.com>
2026-07-25 12:36:56 -0500

    Corregir configuración Render

--- src/main/java/com/bodegazo/ferreteria/config/WebMvcConfig.java
+++ src/main/java/com/bodegazo/ferreteria/config/WebMvcConfig.java
@@ -2,35 +2,45 @@ package com.bodegazo.ferreteria.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-/**
- * Configuración de la capa de vistas (JSP + JSTL) y de los recursos
- * estáticos servidos desde /src/main/webapp y /src/main/resources/static.
- */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public InternalResourceViewResolver viewResolver() {
+    public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class);
         resolver.setPrefix("/WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
-        resolver.setOrder(1);
+        resolver.setOrder(0);
         return resolver;
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/", "classpath:/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/", "classpath:/static/js/");
-        registry.addResourceHandler("/images/**").addResourceLocations("/images/", "classpath:/static/images/");
-        registry.addResourceHandler("/icons/**").addResourceLocations("classpath:/static/icons/");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:src/main/webapp/uploads/");
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+
+        registry.addResourceHandler("/icons/**")
+                .addResourceLocations("classpath:/static/icons/");
+
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/");
+
+        // Render
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/app/uploads/");
     }
 }
